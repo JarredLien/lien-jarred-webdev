5b32e7c3293857ca44cf518f3792c0aaa0fbf832
JarredLien <Assignments>
2017-07-10 18:25:06 -0400

    fixed string

--- test/app.js
+++ test/app.js
@@ -10,7 +10,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.ML_LAB_USERNAME; // get from environment
     var password = process.env.ML_LAB_PASSWORD; // get from environment
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds153352.mlab.com:53352/heroku_4pn40v6h'; // user yours
+    
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
