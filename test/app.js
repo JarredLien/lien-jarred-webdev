28e3d10383ebebbd8d3be354613138e1c8162688
JarredLien <Assignments>
2017-07-27 10:57:36 -0400

    updated vars

--- test/app.js
+++ test/app.js
@@ -8,10 +8,9 @@ app.delete("/api/test/:id", deleteMessage);
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV; // get from environment
     connectionString = 'mongodb://' + username + ':' + password;
     connectionString += '@ds153352.mlab.com:53352/heroku_4pn40v6h'; // user yours
-    
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
