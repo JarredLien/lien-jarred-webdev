f52de3fa035b55194cf9856ab69ffa08c0d0e727
Jarred Lien <jlien15@ccs.neu.edu>
2017-08-13 15:35:09 -0400

    Base for roster

--- public/project/app.js
+++ public/project/app.js
@@ -4,32 +4,4 @@
 (function () {
     angular
         .module("searchApp", ["ngRoute"])
-        // .controller("SearchController", SearchController)
-        // .service("sportService", sportService);
-
-    // function searchController(sportService) {
-    //     var model = this;
-
-    //     model.searchSport = searchSport;
-
-    //     function init() {
-
-    //     }
-
-    //     init();
-
-    //     function searchSport(sport) {
-    //         sportService.searchSport(sport);
-    //     }
-    // }
-
-    function sportService($http) {
-        this.searchSport = searchSport;
-
-        function searchSport(sport) {
-            // var url = URI("http://api.sportradar.us/nfl-ot2/seasontd/2016/standings.xml?api_key=qhr3qrja8x92gdj8uy7ruaz6");
-            return $http.get('http://api.sportradar.us/nfl-ot2/seasontd/2016/standings.xml?api_key=qhr3qrja8x92gdj8uy7ruaz6');
-        }
-    }
-
 })();
