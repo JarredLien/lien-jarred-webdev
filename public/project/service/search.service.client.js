16e9bfe13e89f815649b9efc41e7240de4512c1a
Jarred Lien <jlien15@ccs.neu.edu>
2017-08-14 22:06:15 -0400

    proxy server

--- public/project/service/search.service.client.js
+++ public/project/service/search.service.client.js
@@ -15,23 +15,20 @@
 
             switch(sportId) {
                 case "NFL":
-                    endpoint = "//api.sportradar.us/nfl-ot2/league/hierarchy.json?api_key=xmddybpgv2um2g74uqcsh4qs";
+                    endpoint = "/api/search/NFL";
                     break;
                 case "MLB":
-                    endpoint = "//api.sportradar.us/mlb-t6/league/hierarchy.json?api_key=xw4m2y738ptck73tzhwake7b";
+                    endpoint = "/api/search/MLB";
                     break;
                 case "NHL":
-                    endpoint = "//api.sportradar.us/nhl-ot4/league/hierarchy.json?api_key=ur2jwt7x38pdws7ch635bhmb";
+                    endpoint = "/api/search/NHL";
                     break;
                 case "NBA":
-                    endpoint = "//api.sportradar.us/nba-t3/league/hierarchy.json?api_key=4uvxcvhy62szzwwm6st6ynab";
+                    endpoint = "/api/search/NBA";
                     break;
             }
 
-            $http.get(endpoint)
-                .then(function(res) {
-                    return res;
-                });
+            return $http.get(endpoint);
         }
 
     }
