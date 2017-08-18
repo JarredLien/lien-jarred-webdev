f4d162ce368088059ad99f6025c132d21b14807c
Jarred Lien <jlien15@ccs.neu.edu>
2017-08-18 19:13:56 -0400

    Add team name to roster

--- public/project/views/roster/controllers/roster.controller.client.js
+++ public/project/views/roster/controllers/roster.controller.client.js
@@ -26,9 +26,11 @@
             }
         ]
         $scope.roster = null;
+        $scope.teamName = null;
 
         $scope.init = function() {
             RosterService.findRosterByTeamID($routeParams["sportId"], $routeParams["tid"]).then(function(res) {
+                $scope.teamName = res.data.name;
                 $scope.roster = res.data.players;
             })
         }
