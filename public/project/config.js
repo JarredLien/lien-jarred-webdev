409a0752aafc43e6c7c9d44f75fbe046776c1cbb
JarredLien <Assignments>
2017-08-19 09:16:06 -0400

    updated title

--- public/project/config.js
+++ public/project/config.js
@@ -54,7 +54,7 @@
                 controllerAs: "model"
             })
             .otherwise({
-                redirectTo: "/login"
+                redirectTo: "/landing"
             });
     }
 })();
